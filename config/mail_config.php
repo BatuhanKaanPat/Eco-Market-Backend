c1f72bf60d4ee692becff98fa5c89620055ca2b3
BatuhanKaanPat <batuhan.kaanpat@gmail.com>
2025-06-11 15:21:31 +0300

    Update mail_config.php

--- config/mail_config.php
+++ config/mail_config.php
@@ -5,13 +5,13 @@
  */
 
 // Bilkent Email Credentials - Update with your credentials
-const EMAIL = 'altug.ucar@ug.bilkent.edu.tr';   // Your Bilkent email
-const EMAIL_PASSWORD = '4uecsNoG';                 // Your email password
-const FULLNAME = 'Eco Market';                        // Your name or service name
+const EMAIL = '';                          // Your email
+const EMAIL_PASSWORD = '';                 // Your email password
+const FULLNAME = 'Eco Market';             // Your name or service name
 
 // Bilkent SMTP Server details
-const SMTP_HOST = 'asmtp.bilkent.edu.tr';            // Bilkent SMTP server
-const SMTP_PORT = 587;                               // SMTP port
+const SMTP_HOST = '';            // SMTP server
+const SMTP_PORT = ;              // SMTP port
 const SMTP_SECURITY = PHPMailer\PHPMailer\PHPMailer::ENCRYPTION_STARTTLS;  // Encryption type
 
 // Debug level (0 = no debug output, 1 = client messages, 2 = client and server messages)
